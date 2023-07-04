d79fb6ab11d7cf8f1647409253ac85084e8f20bb
TeoCasanovaDW <teo.casanova13@gmail.com>
2023-07-04 14:46:55 +0200

    dockerfiles

---
+++ frontend/src/views/CookiesGestionView.vue
@@ -0,0 +1,25 @@
+<template>
+    <section class="cookies-gestion">
+        <h1>Gestion des cookies</h1>
+        <ol>
+            <li>Utilisation des cookies : Notre site web utilise des cookies pour améliorer votre expérience de navigation. Les cookies sont de petits fichiers texte stockés sur votre appareil lorsque vous visitez notre site. Ils sont largement utilisés pour permettre au site de fonctionner correctement, d'analyser le trafic, de mémoriser vos préférences et de personnaliser le contenu.</li>
+            <li>Types de cookies utilisés : Nous utilisons les types de cookies suivants sur notre site web :</li>
+            <li>Cookies essentiels : Ces cookies sont nécessaires au bon fonctionnement du site web et vous permettent de naviguer et d'utiliser ses fonctionnalités.</li>
+            <li>Cookies de performance : Ces cookies collectent des informations sur la manière dont vous utilisez notre site web, comme les pages que vous visitez le plus souvent ou les messages d'erreur que vous rencontrez. Ces informations nous aident à améliorer la performance de notre site.</li>
+            <li>Cookies de fonctionnalité : Ces cookies permettent au site web de se souvenir des choix que vous avez faits (par exemple, la langue ou la région) et de vous fournir des fonctionnalités améliorées et personnalisées.</li>
+            <li>Cookies publicitaires : Ces cookies sont utilisés pour diffuser des publicités qui sont pertinentes pour vous et vos intérêts. Ils sont également utilisés pour limiter le nombre de fois que vous voyez une publicité et pour mesurer l'efficacité d'une campagne publicitaire.</li>
+            <li>Contrôle des cookies : Vous pouvez contrôler et gérer les cookies de différentes manières. Vous pouvez les accepter ou les refuser en configurant les paramètres de votre navigateur. Veuillez noter que le refus des cookies peut affecter certaines fonctionnalités de notre site web.</li>
+            <li>Consentement : En utilisant notre site web, vous consentez à l'utilisation des cookies conformément à cette politique de gestion des cookies. Si vous ne consentez pas à l'utilisation de cookies, veuillez configurer les paramètres de votre navigateur en conséquence ou cesser d'utiliser notre site web.</li>
+            <li>Modifications de la politique de gestion des cookies : Nous nous réservons le droit de modifier cette politique de gestion des cookies à tout moment. Toute modification sera publiée sur notre site web et entrera en vigueur immédiatement.</li>
+        </ol>
+    </section>
+</template>
+
+
+<script>
+
+export default{
+  name: 'cookiesGestionView'
+}
+
+</script>
---
+++ frontend/src/views/PrivacyPolicyView.vue
@@ -0,0 +1,24 @@
+<template>
+    <section class="privacy-policy">
+        <h1>Politique de confidentialité</h1>
+        <ol>
+            <li>Collecte d'informations : Nous collectons certaines informations personnelles lorsque vous effectuez une réservation, telles que votre nom, votre adresse, votre numéro de téléphone et votre adresse e-mail. Ces informations sont nécessaires pour gérer votre réservation et vous contacter si nécessaire. </li>
+            <li>Utilisation des informations : Les informations personnelles que nous collectons sont utilisées dans le but de traiter et de confirmer votre réservation, de communiquer avec vous concernant votre séjour, d'améliorer nos services et de répondre à vos demandes. Nous ne partageons pas vos informations personnelles avec des tiers, sauf obligation légale. </li>
+            <li>Sécurité des données : Nous prenons des mesures de sécurité appropriées pour protéger vos informations personnelles contre tout accès non autorisé, toute divulgation, toute altération ou toute destruction. Seules les personnes autorisées ont accès à ces informations, et elles sont tenues de les traiter de manière confidentielle. </li>
+            <li>Conservation des données : Nous conservons vos informations personnelles aussi longtemps que nécessaire pour atteindre les objectifs mentionnés ci-dessus, à moins qu'une période de conservation plus longue ne soit requise ou autorisée par la loi. </li>
+            <li>Cookies : Notre site web utilise des cookies pour améliorer votre expérience en ligne. Les cookies sont de petits fichiers texte qui sont stockés sur votre appareil lorsque vous visitez notre site. Ils ne contiennent aucune information personnelle identifiable et sont utilisés pour des fins techniques et analytiques. Vous pouvez configurer votre navigateur pour refuser les cookies ou être averti lorsque des cookies sont envoyés. </li>
+            <li>Liens vers des sites tiers : Notre site web peut contenir des liens vers des sites web tiers. Veuillez noter que nous ne sommes pas responsables de la collecte, de l'utilisation ou de la divulgation de vos informations personnelles par ces sites. Nous vous encourageons à lire les politiques de confidentialité de ces sites avant de fournir vos informations personnelles. </li>
+            <li>Droits des utilisateurs : Vous avez le droit d'accéder à vos informations personnelles, de les corriger, de les supprimer ou de limiter leur utilisation. Vous pouvez également vous opposer au traitement de vos données personnelles ou demander leur portabilité. Veuillez nous contacter aux coordonnées fournies dans les mentions légales pour exercer vos droits. </li>
+            <li>Modifications de la politique de confidentialité : Nous nous réservons le droit de modifier cette politique de confidentialité à tout moment. Toute modification sera publiée sur notre site web et entrera en vigueur immédiatement.</li>
+        </ol>
+    </section>
+</template>
+
+
+<script>
+
+export default{
+  name: 'privacyPolicy'
+}
+
+</script>
